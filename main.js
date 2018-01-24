38ad6837428317fb518d79dc2a6c9736d60c11fc
Ak-lee <laklc@126.com>
2018-01-24 22:54:51 +0800

    相似代码优化封装 js与html解耦

--- main.js
+++ main.js
@@ -34,47 +34,29 @@ function makeFakeSlides(){
 }
 
 function bindEvents(){
-    $buttons.eq(0).on('click',function(){
-        console.log('current'+current);
-        if(current==5){
-            $slides.css({transform:'translateX(-2240px)'})
+
+    $('#buttonWrapper').on('click','button',function(e){ //点我里面的button才触发
+        let $button = $(e.currentTarget)
+        let index = $button.index();
+        if(current === $buttons.length -1 && index===0){
+            $slides.css({transform:`translateX(${-($buttons.length+1)*320}px)`})
             .one('transitionend',function(){
                 $slides.hide().offset()
             $slides.css({transform:'translateX(-320px)'}).show();
             })
-        }else{
-            $slides.css({transform:'translateX(-320px)'});
-        }
-        current=0;
-        
-    })
-    
-    $buttons.eq(1).on('click',function(){
-        $slides.css({transform:'translateX(-640px)'});
-        current=2;
-    })
-    $buttons.eq(2).on('click',function(){
-        $slides.css({transform:'translateX(-960px)'});
-        cuurent=3;
-    })
-    $buttons.eq(3).on('click',function(){
-        $slides.css({transform:'translateX(-1280px)'});
-        current=4;
-    })
-    $buttons.eq(4).on('click',function(){
-        $slides.css({transform:'translateX(-1600px)'});
-    })
-    
-    $buttons.eq(5).on('click',function(){
-        if(current==0){
+            console.log(1)
+        }else if(current ===0 && index===$buttons.length-1){
             $slides.css({transform:'translateX(0px)'})
             .one('transitionend',function(){
                 $slides.hide().offset()
-            $slides.css({transform:'translateX(-1920px)'}).show();
+            $slides.css({transform:`translateX(${-$buttons.length * 320}px)`}).show();
             })
+            console.log(2)
         }else{
-            $slides.css({transform:'translateX(-1920px)'});
+            $slides.css({transform:`translateX(${-(index+1)*320}px)`});
+            console.log(3)
         }
-        current=5;
+        current = index;
     })
+    
 }
